c3fa87f24a3be24e07c730d0a4f6565ad664c923
aliasgar1978 <aholo2000@gmail.com>
2021-07-28 13:24:37 +0530

    ver

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fwOper",
-    version="0.0.1",
+    version="0.0.2",
     author="ALIASGAR - ALI",
     author_email="aholo2000@gmail.com",
     description="Cisco Firewall Helper Package",
