7069cc983c4ea6c017c4d3de8cab6def4eb05430
aliasgar1978 <aholo2000@gmail.com>
2022-03-15 10:50:01 +0530

    .

--- conf.py
+++ conf.py
@@ -34,7 +34,12 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx_rtd_theme',
-]
+    'sphinx.ext.coverage', 
+    'sphinx.ext.napoleon', 
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
+    'sphinx_rtd_theme',
+    ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
